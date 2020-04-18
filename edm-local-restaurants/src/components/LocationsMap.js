ed85f7e3639965d46585f39fd058fcc8a7bb61ba
Shouyang Zhou <s.zhou1511@gmail.com>
2020-04-18 17:55:28 -0600

    DZ: Refactored a few files

--- edm-local-restaurants/src/components/LocationsMap.js
+++ edm-local-restaurants/src/components/LocationsMap.js
@@ -1,46 +1,60 @@
-import React from 'react';
-import L from 'leaflet'
+import React from "react";
+import L from "leaflet";
 
-import { Map, Marker, Popup, TileLayer, Circle } from 'react-leaflet'
-
+import { Map, Marker, Popup, TileLayer, Circle } from "react-leaflet";
 
 class LocationsMap extends React.Component {
-  // Leaflet map used to display locations. Uses a combination of 'leaflet' and 'react-leaflet' to render. 
+  // Leaflet map used to display locations. Uses a combination of 'leaflet' and 'react-leaflet' to render.
 
   render() {
     const userPosition = [this.props.lat, this.props.lng];
-    const locationsWithMapData = this.props.locations.filter( (location) => {return location.hasLocationData() && location.shouldBeShown})
+    const locationsWithMapData = this.props.locations.filter((location) => {
+      return location.hasLocationData() && location.shouldBeShown;
+    });
 
     return (
       <Map center={userPosition} zoom={this.props.zoom}>
-        <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'/>
-        <Marker position={userPosition} draggable={true} onDragend={this.props.handleMarkerDragEnd} />
-        <Circle center={userPosition} radius={this.props.filterRadius * 1000}></Circle>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker
+          position={userPosition}
+          draggable={true}
+          onDragend={this.props.handleMarkerDragEnd}
+        />
+        <Circle center={userPosition} radius={this.props.filterRadius * 1000} />
         {locationsWithMapData.map(this.renderLocationMarker.bind(this))}
       </Map>
-    );    
+    );
   }
 
   static greenIcon = new L.Icon({
-    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconUrl:
+      "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+    shadowUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
-    shadowSize: [41, 41]
+    shadowSize: [41, 41],
   });
 
   renderLocationMarker(location) {
     // Helper function to render location map markers.
     // const distanceToLocation = location.distanceToLocationMeters(this.props.map.lat, this.props.map.lng);
     return (
-      <Marker position={[location.lat, location.long]} icon={LocationsMap.greenIcon}>
-        <Popup>{location.name}  {location.distanceToLocationMeters(this.props.lat, this.props.lng)}
+      <Marker
+        position={[location.lat, location.long]}
+        icon={LocationsMap.greenIcon}
+      >
+        <Popup>
+          {location.name}{" "}
+          {location.distanceToLocationMeters(this.props.lat, this.props.lng)}
         </Popup>
       </Marker>
-    )
+    );
   }
-
 }
 
 export default LocationsMap;
--- edm-local-restaurants/src/models/Location.js
+++ edm-local-restaurants/src/models/Location.js
@@ -1,56 +1,78 @@
-function degreesToRadians(degrees) {
-  return degrees * Math.PI / 180;
-}
-
 function distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
-  var earthRadiusKm = 6371;
+  const { sin, cos, sqrt, atan2, PI } = Math;
+  function degreesToRadians(degrees) {
+    return (degrees * PI) / 180;
+  }
 
-  var dLat = degreesToRadians(lat2-lat1);
-  var dLon = degreesToRadians(lon2-lon1);
+  const earthRadiusKm = 6371;
 
-  lat1 = degreesToRadians(lat1);
-  lat2 = degreesToRadians(lat2);
+  const dLat = degreesToRadians(lat2 - lat1);
+  const dLon = degreesToRadians(lon2 - lon1);
 
-  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-          Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  const lat1x = degreesToRadians(lat1);
+  const lat2x = degreesToRadians(lat2);
+
+  const a =
+    sin(dLat / 2) * sin(dLat / 2) +
+    sin(dLon / 2) * sin(dLon / 2) * cos(lat1x) * cos(lat2x);
+  const c = 2 * atan2(sqrt(a), sqrt(1 - a));
   return earthRadiusKm * c;
 }
 
 class Location {
-    // Data class representing one row from the Google Sheets Document.
-    constructor(entry) {
-      // Unpacks interesting entries from the Google Sheets JSON API into a simpler object.
-      this.id = entry.id.$t;
-      this.shouldBeShown = true;
-  
-      this.name                = entry["gsx$name"].$t;
-      this.status              = entry["gsx$status"].$t;
-      this.cusine              = entry["gsx$cusine"].$t;
-      this.area                = entry["gsx$area"].$t;
-      this.hours               = entry["gsx$hours-unsureofcovid-19hours"].$t;
-      this.delivery            = entry["gsx$delivery"].$t;
-      this.pickup              = entry["gsx$pickup"].$t;
-      this.giftcards           = entry["gsx$giftcards"].$t;
-      this.dietaryNeeds        = entry["gsx$dietaryneedsserved"].$t;
-      this.address             = entry["gsx$address"].$t;
-      this.comments            = entry["gsx$comments"].$t;
-  
-      this.lat                 = entry["gsx$lat"].$t;
-      this.long                = entry["gsx$long"].$t;
-    }
-  
+  // Data class representing one row from the Google Sheets Document.
+  static GSXKeys = [
+    "name",
+    "status",
+    "cusine",
+    "area",
+    "hours-unsureofcovid-19hours",
+    "delivery",
+    "pickup",
+    "giftcards",
+    "dietaryneedsserved",
+    "address",
+    "comments",
+    "lat",
+    "long",
+  ];
 
-    hasLocationData() {
-      return (!!this.lat && !!this.long);
+  constructor(entry) {
+    function getGSXValue(key) {
+      return entry[`gsx$${key}`].$t;
     }
+    // Unpacks interesting entries from the Google Sheets JSON API into a simpler object.
+    this.id = entry.id.$t;
+    this.shouldBeShown = true;
 
-    distanceToLocationMeters(lat, long) { 
-      return distanceInKmBetweenEarthCoordinates(lat,long, this.lat, this.long) * 1000;
-    }
+    this.name = getGSXValue("name");
+    this.status = getGSXValue("status");
+    this.cusine = getGSXValue("cusine");
+    this.area = getGSXValue("area");
+    this.hours = getGSXValue("hours-unsureofcovid-19hours");
+    this.delivery = getGSXValue("delivery");
+    this.pickup = getGSXValue("pickup");
+    this.giftcards = getGSXValue("giftcards");
+    this.dietaryNeeds = getGSXValue("dietaryneedsserved");
+    this.address = getGSXValue("address");
+    this.comments = getGSXValue("comments");
 
-    // TODO: Probably add some functionality to unpack attributes (addresses, hours, cusine types etc...)
-    // TODO: Maybe add resolvers to Google Maps, Food Delivery etc...
+    this.lat = getGSXValue("lat");
+    this.long = getGSXValue("long");
   }
-  
-export default Location; 
+
+  hasLocationData() {
+    return !!this.lat && !!this.long;
+  }
+
+  distanceToLocationMeters(lat, long) {
+    return (
+      distanceInKmBetweenEarthCoordinates(lat, long, this.lat, this.long) * 1000
+    );
+  }
+
+  // TODO: Probably add some functionality to unpack attributes (addresses, hours, cusine types etc...)
+  // TODO: Maybe add resolvers to Google Maps, Food Delivery etc...
+}
+
+export default Location;
