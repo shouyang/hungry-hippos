e6290ab674629882e8e67b373e5ddc66c079a2db
Shouyang Zhou <s.zhou1511@gmail.com>
2020-05-09 14:59:58 -0600

    DZ: Reviewed Pageheader text

--- edm-local-restaurants/src/components/PageHeader.js
+++ edm-local-restaurants/src/components/PageHeader.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Accordion, Icon } from "semantic-ui-react";
+import { Accordion, Icon, Label } from "semantic-ui-react";
 
 class PageHeader extends React.Component {
   state = { activeIndex: 0 };
@@ -25,21 +25,25 @@ class PageHeader extends React.Component {
         <Accordion.Content active={activeIndex === 0}>
           <p>Hello friends,</p>
           <p>
-            Hello everyone! As you may know, the COVID19 crsis is hitting everyone hard, especially small businesses. A friend and I have put together a website listing many Asian restaurants in the Edmonton Area that still do delivery or pickups, because they have been hit particularly hard. This is not a comprehensive list of all Asian restarunts in Edmonton, so please feel free to use the Google sheet to submit more. 
-
-            As the Alberta economy reopens in the coming months and after the COVID19 pandemic subsides, we hope this website will be a contineous way to share, support, and order from delicious Asian restaurants across Alberta.
+            As you may know, the COVID19 crsis is hitting everyone hard, especially small businesses. We've
+            put together a website listing many Asian restaurants in the Edmonton Area that still do delivery
+            or pickups, because they have been hit particularly hard. This is not a comprehensive list of all
+            Asian restarunts in Edmonton, we've been tracking via Google Sheets. Please feel free to submit
+            more below. As the Alberta economy reopens in the coming months and after the COVID19 pandemic
+            subsides, we hope this website will be a contineous way to share, support, and order from
+            delicious Asian restaurants across Alberta.
           </p>
           <p>Click on the pins or on a row in the table to get started!</p>
-        </Accordion.Content>
-        <Accordion.Title active={activeIndex === 1} index={1} onClick={this.handleClick}>
-          About
-          <Icon name="dropdown" />
-        </Accordion.Title>
-        <Accordion.Content active={activeIndex === 1}>
-          <p>
-            You can drag the blue marker below to find nearby restaurants. You can also use the "My Location
-            button" on compatible devices to go to your current location too!
-          </p>
+
+          <Label
+            as="a"
+            href="https://docs.google.com/spreadsheets/d/1YvbMUEEGha3hEQ6CDd0K1C4WVkAMmszbBRuRQ-ndxKU/edit?fbclid=IwAR0-PcTBQiVML5SDvJGDhYxiY3ya_gQOv8hQOKg4sX94gZpwvJW6hEnuLPE#gid=0"
+          >
+            Google Sheets
+          </Label>
+          <Label as="a" href="https://github.com/shouyang/hungry-hippos">
+            Github
+          </Label>
         </Accordion.Content>
       </Accordion>
     );
